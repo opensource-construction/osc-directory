b7d14f29daf4f92cf1495e904861ddf36ac580b5
Felix Brunold <48569186+TheVessen@users.noreply.github.com>
2025-08-24 17:41:02 +0200

    add friendly

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -6,6 +6,26 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<script>
+		var _paq = (window._paq = window._paq || []);
+		_paq.push(['setExcludedQueryParams', ['_gl', 'gad_source', 'wbraid', 'gad_campaignid']]);
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function () {
+			var u = 'https://app.friendlyanalytics.ch/';
+			_paq.push(['setTrackerUrl', u + 'js/tracker.php']);
+			_paq.push(['setSiteId', '294']);
+			var d = document,
+				g = d.createElement('script'),
+				s = d.getElementsByTagName('script')[0];
+			g.async = true;
+			g.src = u + 'js/tracker.php';
+			s.parentNode.insertBefore(g, s);
+		})();
+	</script>
+</svelte:head>
+
 <div class="flex flex-col min-h-screen">
 	<Nav />
 
